4daf30ed8eb3f0a591f7753c7ee1d82cc930d909
vitaliivepsha <vitaly.vepsha@gmail.com>
2022-05-06 16:14:23 +0300

    PLATF-385

--- app/app.js
+++ app/app.js
@@ -60,6 +60,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('./assets/templates/layouts/student-stages-upload-photo.html');
   require('./assets/templates/layouts/student-stages-accompaniment.html');
   require('./assets/templates/layouts/student-stages-employer.html');
+  require('./assets/templates/layouts/student-stages-ready-to-go.html');
   require('./assets/templates/layouts/student-cabinet-trips.html');
   require('./assets/templates/layouts/student-cabinet-company-information.html');
   require('./assets/templates/layouts/company-cabinet-information.html');
